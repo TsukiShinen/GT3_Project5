0daa906984a4afb7c79dd4b03832fb6357546c31
OVERClock <40008329+OV3RClock@users.noreply.github.com>
2023-01-30 12:55:40 +0100

    End of class 30/01

--- Source/GT3_Project5_Gr1/EnemySkeleton.cpp
+++ Source/GT3_Project5_Gr1/EnemySkeleton.cpp
@@ -10,16 +10,21 @@ AEnemySkeleton::AEnemySkeleton()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	/*
-	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
-	SkeletalMesh->SetupAttachment(RootComponent);
-	SkeletalMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));*/
+
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	TriggerBox->SetupAttachment(RootComponent);
+	TriggerBox->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AEnemySkeleton::OnOverlapBegin);
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AEnemySkeleton::OnOverlapEnd);
+
 }
 
 // Called when the game starts or when spawned
 void AEnemySkeleton::BeginPlay()
 {
 	Super::BeginPlay();
+	AnimInstance = GetMesh()->GetAnimInstance();
 }
 
 // Called every frame
@@ -56,4 +61,24 @@ float AEnemySkeleton::TakeDamage(float Damage, const FDamageEvent& DamageEvent, 
 	}
 
 	return Super::TakeDamage(Damage, DamageEvent, EventInstigator, DamageCauser);
+}
+
+void AEnemySkeleton::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (auto Player = Cast<AGT3_Project5_Gr1Character>(OtherActor))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("ENTER"));
+		IsAttacking = true;
+		PlayAttackAnim();
+	}
+}
+
+void AEnemySkeleton::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (auto Player = Cast<AGT3_Project5_Gr1Character>(OtherActor))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("QUIT"));
+		IsAttacking = false;
+		StopAttackAnim();
+	}
 }
--- Source/GT3_Project5_Gr1/EnemySkeleton.h
+++ Source/GT3_Project5_Gr1/EnemySkeleton.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Components/BoxComponent.h"
 #include "EnemySkeleton.generated.h"
 
 UCLASS()
@@ -20,16 +21,34 @@ class GT3_PROJECT5_GR1_API AEnemySkeleton : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AEnemySkeleton();
-	/*
-	UPROPERTY(EditAnywhere)
-		class USkeletalMeshComponent* SkeletalMesh;*/
 
-
+	UPROPERTY(VisibleAnywhere)
 	bool IsAlive = true;
+	UPROPERTY(VisibleAnywhere)
+	bool IsAttacking = false;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(VisibleAnywhere)
+	UAnimInstance* AnimInstance;
+
+	UPROPERTY(EditAnywhere)
+	UAnimMontage* AM_Skeleton_Attack;
+
+	void PlayAttackAnim() const { AnimInstance->Montage_Play(AM_Skeleton_Attack); }
+	void StopAttackAnim() const { AnimInstance->Montage_Stop(0.5f, AM_Skeleton_Attack); }
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* TriggerBox;
 
 public:
 	// Called every frame
@@ -38,6 +57,5 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	virtual float TakeDamage(float Damage, const FDamageEvent& DamageEvent, AController* EventInstigator,
-	                         AActor* DamageCauser) override;
+	virtual float TakeDamage(float Damage, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
 };
